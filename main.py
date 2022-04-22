19b2b964f85d9cc1a05ef2a9af6fba0c3411a973
Josh Mandzak <jmandzak99@gmail.com>
2022-04-22 18:29:02 -0400

    changed encoding to example from class

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 
 def get_data(filename, window_size, stride):
     f = open(filename, 'r')
     text = f.read()
 
-    mapping = {}
-    unique_char = 0
+    # mapping = {}
+    # unique_char = 0
     encoded = []
     
-    for c in text:
-        if c in mapping:
-            encoded.append(mapping[c])
-        else:
-            mapping[c] = unique_char
-            unique_char += 1
-            encoded.append(mapping[c])
+    # for c in text:
+    #     if c in mapping:
+    #         encoded.append(mapping[c])
+    #     else:
+    #         mapping[c] = unique_char
+    #         unique_char += 1
+    #         encoded.append(mapping[c])
+
+    # taken from example rnn code
+    chars = sorted(list(set(text)))
+    mapping = dict((c, i) for i, c in enumerate(chars))
+    encoded = np.array([mapping[char] for char in text])
+    print(encoded)
 
     starting_index = 0
     all_slices = []
@@ -38,12 +45,13 @@ def get_data(filename, window_size, stride):
         y_slices.append(string_y)
 
     # print(x_slices)
-    # print(len(y_slices))
-    # print(str(x_slices[0]))
+    print(len(y_slices))
+    print(str(x_slices[0]))
+    print(str(y_slices[0]))
 
-    encoder = OneHotEncoder()
-    encoded = encoder.fit_transform(x_slices)
-    print((encoded.toarray()).shape)
+    # encoder = OneHotEncoder()
+    # encoded = encoder.fit_transform(x_slices)
+    # print((encoded.toarray()).shape)
 
 def main():
     get_data('beatles.txt', 5, 1)
