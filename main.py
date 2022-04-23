24757dd6e50785c199dd697f04e86278f92ae9bc
Josh Mandzak <jmandzak99@gmail.com>
2022-04-23 12:55:55 -0400

    get_data is (hopefully) done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import OneHotEncoder
+from tensorflow.keras.utils import to_categorical
 import numpy as np
 
 
@@ -6,55 +7,39 @@ def get_data(filename, window_size, stride):
     f = open(filename, 'r')
     text = f.read()
 
-    # mapping = {}
-    # unique_char = 0
     encoded = []
-    
-    # for c in text:
-    #     if c in mapping:
-    #         encoded.append(mapping[c])
-    #     else:
-    #         mapping[c] = unique_char
-    #         unique_char += 1
-    #         encoded.append(mapping[c])
 
     # taken from example rnn code
     chars = sorted(list(set(text)))
     mapping = dict((c, i) for i, c in enumerate(chars))
     encoded = np.array([mapping[char] for char in text])
-    print(encoded)
 
+    # create the slices
     starting_index = 0
     all_slices = []
     while(starting_index + window_size+1 < len(encoded)):
         all_slices.append(encoded[starting_index:starting_index+window_size+1])
-        starting_index += window_size+1
+        starting_index += stride
     
+    # turn the slices into x slices and y slices
     x_slices = []
     y_slices = []
     for slice in all_slices:
         x = slice[:-1]
         y = slice[1:]
-        string_x = [str(int) for int in x]
-        string_y = [str(int) for int in y]
-        
-        string_x = "".join(string_x)
-        string_y = "".join(string_y)
 
-        x_slices.append(string_x)
-        y_slices.append(string_y)
+        x_slices.append(x)
+        y_slices.append(y)
 
-    # print(x_slices)
-    print(len(y_slices))
-    print(str(x_slices[0]))
-    print(str(y_slices[0]))
 
-    # encoder = OneHotEncoder()
-    # encoded = encoder.fit_transform(x_slices)
-    # print((encoded.toarray()).shape)
+    # one hot encode the slices and return them
+    x = np.array(to_categorical(x_slices))
+    y = np.array(to_categorical(y_slices))
+
+    return x, y
 
 def main():
-    get_data('beatles.txt', 5, 1)
+    x, y = get_data('beatles.txt', 5, 3)
 
 if __name__ == '__main__':
     main()
